b5a0be87a11ce2f59d1c729b07a5906a28ced391
aksharkh <aksharkh04@gmail.com>
2025-03-15 09:55:02 +0530

    changed appointment model

--- models/Appointment.js
+++ models/Appointment.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const appointmentSchema = new mongoose.Schema({
   student: { 
     type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User', required: true },
+    ref: 'User', 
+    required: true },
   professor: { 
     type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User', required: true },
+    ref: 'User', 
+    required: true },
   availability: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'Availability', 
